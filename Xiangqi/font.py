dc9a01a69cdbe8a283592dc0b23920ec6f265c3d
Entropy-Max <newcastle.yinghui@gmail.com>
2025-12-01 23:40:40 +0000

    Add font

---
+++ Xiangqi/font.py
@@ -0,0 +1,44 @@
+
+# Download manually 
+#!wget -O BabelStoneXiangqiColour.ttf https://raw.githubusercontent.com/Entropy-Max/XiangQi/main/BabelStoneXiangqiColour.ttf
+
+def font_download(source='github')
+    """Download Font BabelStone Xiangqi Colour"""
+
+    import os
+    import requests
+    import shutil
+
+    # Download font
+
+    if source=='github':
+        url = "https://raw.githubusercontent.com/Entropy-Max/XiangQi/main/BabelStoneXiangqiColour.ttf"
+    else: 
+        url = "https://www.babelstone.co.uk/Fonts/Download/BabelStoneXiangqiColour.ttf"
+
+    file_name = "BabelStoneXiangqiColour.ttf"
+
+    r = requests.get(url)
+    with open(file_name, "wb") as f:
+        f.write(r.content)
+
+    print("Font download: ready", file_name)
+
+def font_setup()
+    """Check font file exists, if not then download"""
+
+    import os
+
+    file_path = "BabelStoneXiangqiColour.ttf"
+
+    if !os.path.exists(file_path): font_download()
+       
+    # Activate 
+
+    font_path = "/usr/share/fonts/truetype/BabelStoneXiangqiColour.ttf"
+
+    shutil.copy(file_name, font_path)
+
+    os.system('!fc-cache -f -v')
+    os.system('!fc-list :family')
+    os.system('!fc-list :family | grep -i BabelStoneColour')
