666ff7a7cbae66d7100f028a0eee9727a12ada60
Entropy-Max <64759341+Entropy-Max@users.noreply.github.com>
2025-12-04 17:21:21 +1100

    Update font.py

--- Xiangqi/font.py
+++ Xiangqi/font.py
@@ -3,8 +3,13 @@ import os
 import requests
 import shutil
 
+file_name = "BabelStoneXiangqiColour.ttf"
+font_path = ""
+
 def font_download(source='github'):
     """Download Font BabelStone Xiangqi Colour"""
+
+    global file_name,font_path
     
     # Download font
 
@@ -13,14 +18,14 @@ def font_download(source='github'):
     else: 
         url = "https://www.babelstone.co.uk/Fonts/Download/BabelStoneXiangqiColour.ttf"
 
-    file_name = "BabelStoneXiangqiColour.ttf"
-
     r = requests.get(url)
     with open(file_name, "wb") as f:
         f.write(r.content)
 
     # Activate 
-    font_path = "/usr/share/fonts/truetype/BabelStoneXiangqiColour.ttf"
+    font_path = font_path + '/' + file_name
+    #"/usr/share/fonts/truetype/BabelStoneXiangqiColour.ttf"
+   
     shutil.copy(file_name, font_path)
 
     os.system('!fc-cache -f -v')
@@ -31,11 +36,12 @@ def font_download(source='github'):
 
 def font_setup():
     """Check font file exists, if not then download"""
- 
-    file_name = "BabelStoneXiangqiColour.ttf"
+
+    global file_name,font_path
 
     # System fonts directory
     font_files = fm.findSystemFonts()
+    font_path = os.path.dirname(font_files[0])
     font_names = sorted({os.path.basename(f) for f in font_files})
 
     if (not file_name in font_names) and (not os.path.exists(file_name)):
