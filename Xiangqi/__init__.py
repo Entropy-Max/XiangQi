09a2338b93e03d99760452890ccb0ece0acd8789
Entropy-Max <64759341+Entropy-Max@users.noreply.github.com>
2025-12-06 17:56:00 +1100

    Update __init__.py

--- Xiangqi/__init__.py
+++ Xiangqi/__init__.py
@@ -3,8 +3,9 @@ from Xiangqi.FEN import FEN
 from Xiangqi.Move import Move
 from Xiangqi.UCIEngine import UCIEngine
 
-__all__ = ["FEN","Move","Engine"]
+__all__ = ["FEN","Move","UCIEngine"]
 
 font.font_setup()
+
 
 
