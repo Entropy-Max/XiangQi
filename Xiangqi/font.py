5ab2ca83cf2f882cb49aba3c2874d91a00a56214
Entropy-Max <64759341+Entropy-Max@users.noreply.github.com>
2025-12-06 18:47:51 +1100

    Update font.py

--- Xiangqi/font.py
+++ Xiangqi/font.py
@@ -4,7 +4,7 @@ import requests
 import shutil
 
 file_names = ["BabelStoneXiangqiColour.ttf",
-              "minixianqqi.ttf"]
+              "minixiangqi.ttf"]
 font_path = ""
 
 def font_download(file_name, source='github'):
