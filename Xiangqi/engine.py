71628af08562a2826895df89e0796479a22b6e7f
Entropy-Max <64759341+Entropy-Max@users.noreply.github.com>
2025-12-07 21:48:26 +1100

    Create engine.py

---
+++ Xiangqi/engine.py
@@ -0,0 +1,29 @@
+import os
+
+engine_path = '/content/fairyxq'
+file_name = os.path.basename(engine_path)
+
+def fairy_download(source="xiangqi"):
+  
+    global file_name
+  
+    if source.lower()=='xiangqi':
+        url = "https://raw.githubusercontent.com/Entropy-Max/XiangQi/main/fairyxq"
+    else:
+        url = "https://github.com/fairy-stockfish/Fairy-Stockfish/releases/latest/download/fairy-stockfish-largeboard_x86-64"
+
+    r = requests.get(url)
+    with open(file_name, "wb") as f:
+        f.write(r.content)
+    
+    print("Fairy Stockfish downloading ...... done!")
+
+def fairy_setup()
+
+  global engine_path
+  
+  if not os.path.exists(engine_path):
+      fairy_download()
+  
+  # Make it executable
+  os.system("chmod +x {file_name}")
