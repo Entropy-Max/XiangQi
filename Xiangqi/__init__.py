1c8f29df26950fa4f64692e22eee470855507c76
Entropy-Max <64759341+Entropy-Max@users.noreply.github.com>
2025-12-12 19:28:35 +1100

    Update __init__.py

--- Xiangqi/__init__.py
+++ Xiangqi/__init__.py
@@ -4,9 +4,10 @@ from Xiangqi.FEN import FEN
 
 START_FEN = "rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RNBAKABNR r"
 ENGINE_PATH = 'fairyxq'
-NNUE_PATH = './xiangqi-c07e94a5c7cb.nnue'
+NNUE_PATH = 'xiangqi-c07e94a5c7cb.nnue'
 
 __all__ = ["FEN","START_FEN","ENGINE_PATH","NNUE_PATH"]
 
 font.font_setup()
+
 
