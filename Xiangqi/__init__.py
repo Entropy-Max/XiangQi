9221d3615161e37253b5293493f565c9ddb508ba
Entropy-Max <64759341+Entropy-Max@users.noreply.github.com>
2025-12-16 15:03:38 +1100

    Update __init__.py

--- Xiangqi/__init__.py
+++ Xiangqi/__init__.py
@@ -1,12 +1,13 @@
 from . import common
-from common import *
 from . import font
 from . import FEN
+from Xiangqi.common import *
 from Xiangqi.FEN import FEN
 
 __all__ = ["FEN","START_FEN","ENGINE_PATH","NNUE_PATH"]
 
 font.font_setup()
+
 
 
 
