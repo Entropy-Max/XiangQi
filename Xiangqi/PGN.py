a86ecfd07afb11e0c2f6703782ac3e32fa6fe4c0
Entropy-Max <64759341+Entropy-Max@users.noreply.github.com>
2025-12-16 16:34:52 +1100

    Update PGN.py

--- Xiangqi/PGN.py
+++ Xiangqi/PGN.py
@@ -1,4 +1,4 @@
-from Xiangqi.common import piece_map_etc, numerals_english, action_map_etc
+from Xiangqi.common import piece_map_cte, piece_map_etc, numerals_english, numerals_chinese, action_map_etc,action_map_cte
 
 def AXF_CHN(moves):
 
@@ -29,3 +29,36 @@ def AXF_CHN(moves):
 
     return '\n'.join(move_new)
           
+def CHN_AXF(moves):
+
+    move_new = []
+
+    for move in moves.strip().split('\n'):
+        result = move.split()
+        
+        round = result[0]
+        
+        red_move = result[1]   
+        if len(red_move)==4:
+
+            red_move=piece_map_cte[red_move[0]]+\
+     numerals_chinese[red_move[1]]+\
+     action_map_cte[red_move[2]]+\
+     numerals_chinese[red_move[3]]
+
+        if len(result) == 3: 
+            black_move = result[2]
+            black_move = piece_map_cte[black_move[0]]+\
+     black_move[1]+\
+     action_map_cte[black_move[2]]+\
+     black_move[3]
+
+            move_new.append(round+' '+red_move+' '+black_move)
+
+        else:
+            move_new.append(round+' '+red_move)
+
+    return '\n'.join(move_new)
+
+            
+
