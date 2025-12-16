b30417b37a07b6377fb5732e1e96f99363b7161f
Entropy-Max <64759341+Entropy-Max@users.noreply.github.com>
2025-12-16 19:07:04 +1100

    Update PGN.py

--- Xiangqi/PGN.py
+++ Xiangqi/PGN.py
@@ -11,16 +11,16 @@ def AXF_CHN(moves):
         if len(red_move)==4:
 
             red_move=AXF_map_etc[red_move[0]]+\
-     numerals_english[int(red_move[1])]+\
-     action_map_etc[red_move[2]]+\
-     numerals_english[int(red_move[3])]
+                numerals_english[int(red_move[1])]+\
+                action_map_etc[red_move[2]]+\
+                numerals_english[int(red_move[3])]
 
         if len(result) == 3: 
             black_move = result[2]
             black_move = AXF_map_etc[black_move[0].lower()]+\
-     black_move[1]+\
-     action_map_etc[black_move[2]]+\
-     black_move[3]
+                black_move[1]+\
+                action_map_etc[black_move[2]]+\
+                black_move[3]
 
             move_new.append(round+' '+red_move+' '+black_move)
 
@@ -42,16 +42,16 @@ def CHN_AXF(moves):
         if len(red_move)==4:
 
             red_move=AXF_map_cte[red_move[0]]+\
-     str(numerals_chinese[red_move[1]])+\
-     action_map_cte[red_move[2]]+\
-     str(numerals_chinese[red_move[3]])
+                str(numerals_chinese[red_move[1]])+\
+                action_map_cte[red_move[2]]+\
+                str(numerals_chinese[red_move[3]])
 
         if len(result) == 3: 
             black_move = result[2]
             black_move = AXF_map_cte[black_move[0]]+\
-     str(black_move[1])+\
-     action_map_cte[black_move[2]]+\
-     str(black_move[3])
+                 str(black_move[1])+\
+                 action_map_cte[black_move[2]]+\
+                 str(black_move[3])
 
             move_new.append(round+' '+red_move+' '+black_move)
 
@@ -59,6 +59,3 @@ def CHN_AXF(moves):
             move_new.append(round+' '+red_move)
 
     return '\n'.join(move_new)
-
-            
-
