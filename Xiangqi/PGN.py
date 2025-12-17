d4ab9d4b337b4b0eaece40a80203b47d47046daa
Entropy-Max <64759341+Entropy-Max@users.noreply.github.com>
2025-12-17 12:28:48 +1100

    Update PGN.py

--- Xiangqi/PGN.py
+++ Xiangqi/PGN.py
@@ -79,16 +79,16 @@ class PGN():
 
                 red_move=red_move[0]+\
                     str(10 - numerals_chinese[red_move[1]]) if red_move[2] == '平' else  str(numerals_chinese[red_move[1]]) +\
-                    str(numerals_chinese[red_move[2]])+\
+                    red_move[2]+\
                     str(10 - numerals_chinese[red_move[3]]) if red_move[2] == '平' else  str(numerals_chinese[red_move[3]]) 
                     
 
             if len(result) == 3: 
                 black_move = result[2]
                 black_move=black_move[0]+\
-                    str(10 - numerals_chinese[black_move[1]]) if black_move[2] == '平' else  str(numerals_chinese[black_move[1]]) +\
-                    str(numerals_chinese[black_move[2]])+\
-                    str(10 - numerals_chinese[black_move[3]]) if black_move[2] == '平' else  str(numerals_chinese[black_move[3]]) 
+                    str(10 - black_move[1]) if black_move[2] == '平' else  str(black_move[1]) +\
+                    black_move[2]+\
+                    str(10 - black_move[3]) if black_move[2] == '平' else  str(black_move[3]) 
                     
 
                 moves_new.append(round+' '+red_move+' '+black_move)
