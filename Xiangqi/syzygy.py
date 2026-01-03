79d0455a452571d748fde2fe2234952fe92e659e
Entropy-Max <64759341+Entropy-Max@users.noreply.github.com>
2026-01-03 11:02:52 +1100

    Create syzygy.py

---
+++ Xiangqi/syzygy.py
@@ -0,0 +1,36 @@
+from Xiangqi import *
+
+def heatmap(BASE_FEN_0,BASE_FEN_1,piece,NNUE=none):
+
+    if NNUE:
+        base_eval = engine._nnue_eval_fen(BASE_FEN_0)
+    else:
+        base_eval = engine._eval_fen(BASE_FEN_0)
+
+
+    # heatmap[rank][file] # rank 0..9, file 0..8
+
+    heatmap = [[0.0 for _ in range(9)] for _ in range(10)]
+
+    for rank in range(10):
+        for file in range(9):
+            fen=FEN(BASE_FEN_1)
+            fen._to_matrix()
+
+            r = 9 - rank
+
+            if fen.board[r][file]== '.':
+                fen.board[r][file]=piece
+
+                fen._from_matrix()
+
+                if NNUE:
+                    v = engine._nnue_eval_fen(fen.fen)-base_eval
+                else:
+                    v = engine._eval_fen(fen.fen)-base_eval
+            else:
+                v = 0
+
+            heatmap[rank][file] = v
+          
+    return heatmap
